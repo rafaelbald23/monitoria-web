829b3058680a06f556cba0a8836ce29c4be77df6
rafaelbald23 <rafaeltheobald.med@gmail.com>
2026-01-15 00:33:41 -0300

    fix: remove Employees.tsx, redirect /employees to Users page

--- src/App.tsx
+++ src/App.tsx
@@ -90,7 +90,7 @@ function App() {
           path="/employees"
           element={
             <ProtectedRoute>
-              <Settings />
+              <Users />
             </ProtectedRoute>
           }
         />
